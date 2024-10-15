5d23b4a56d2ae5c1087394cdee95113e7f7ec496
Iván Villagrasa Pérez <56278092+ivvil@users.noreply.github.com>
2024-10-15 10:23:27 +0200

    Start adding configuration

--- preferences/wallpaper.js
+++ preferences/wallpaper.js
@@ -3,19 +3,22 @@ import Gio from 'gi://Gio';
 import GObject from 'gi://GObject';
 
 export class Wallpapers extends Adw.PreferencesPage {
-  static {
-	GObject.registerClass({
-	  GTypeName: 'WallpapersPage',
-	  Template: 'resource://org/gnome'
-	})
-  }
+	static {
+		GObject.registerClass({
+			GTypeName: 'WallpapersPage',
+			Template: 'resource://org/gnome',
+			InternalChildren: [
+				'dark_chooser',
+				'light_chooser',
+			],
+		}, this)
+	}
 
-		constructor({ ...params } = {}) {
+	constructor({ settings, ...params } = {}) {
 		super(params);
 
-		const settings = new Gio.Settings({ schema: 'org.gnome.shell.extensions.gwppm' });
-
-
+		settings.bind('light-wallpapers', this._light_chooser, 'uri', SettingsBindFlags.DEFAULT);
+		settings.bind('dark-wallpapers', this._dark_chooser, 'uri', SettingsBindFlags.DEFAULT);
 	}
 
 
--- prefs.js
+++ prefs.js
@@ -1,48 +1,52 @@
 import Gtk from 'gi://Gtk?version=4.0';
 import Adw from 'gi://Adw';
 
-import {ExtensionPreferences, gettext as _} from 'resource:///org/gnome/Shell/Extensions/js/extensions/prefs.js';
-import {Preferences} from '../../.emacs.d/cache/backups/!home!ivvil!Proyectos!GWppM!preferences!wallpaper.js~/index.js';
+import { ExtensionPreferences, gettext as _ } from 'resource:///org/gnome/Shell/Extensions/js/extensions/prefs.js';
+import { Preferences } from './preferences/wallpaper.js';
 
 
 export default class ExamplePreferences extends ExtensionPreferences {
-    /**
-     * This class is constructed once when your extension preferences are
-     * about to be opened. This is a good time to setup translations or anything
-     * else you only do once.
-     *
-     * @param {ExtensionMeta} metadata - An extension meta object
-     */
-    constructor(metadata) {
-        super(metadata);
+	/**
+	 * This class is constructed once when your extension preferences are
+	 * about to be opened. This is a good time to setup translations or anything
+	 * else you only do once.
+	 *
+	 * @param {ExtensionMeta} metadata - An extension meta object
+	 */
+	constructor(metadata) {
+		super(metadata);
 
-        // console.debug(`constructing ${this.metadata.name}`);
-    }
+		// console.debug(`constructing ${this.metadata.name}`);
+	}
 
-    /**
-     * This function is called when the preferences window is first created to
-     * build and return a GTK4 widget.
-     *
-     * The preferences window will be a `Adw.PreferencesWindow`, and the widget
-     * returned by this function will be added to an `Adw.PreferencesPage` or
-     * `Adw.PreferencesGroup` if necessary.
-     *
-     * @returns {Gtk.Widget} the preferences widget
-     */
-    getPreferencesWidget() {
-        // return new Gtk.Label({
-        //     label: this.metadata.name,
-        // });
-    }
+	/**
+	 * This function is called when the preferences window is first created to
+	 * build and return a GTK4 widget.
+	 *
+	 * The preferences window will be a `Adw.PreferencesWindow`, and the widget
+	 * returned by this function will be added to an `Adw.PreferencesPage` or
+	 * `Adw.PreferencesGroup` if necessary.
+	 *
+	 * @returns {Gtk.Widget} the preferences widget
+	 */
+	getPreferencesWidget() {
+		// return new Gtk.Label({
+		//     label: this.metadata.name,
+		// });
+	}
 
-    /**
-     * Fill the preferences window with preferences.
-     *
-     * If this method is overridden, `getPreferencesWidget()` will NOT be called.
-     *
-     * @param {Adw.PreferencesWindow} window - the preferences window
-     */
-    fillPreferencesWindow(window) {
-	  
-    }
+	/**
+	 * Fill the preferences window with preferences.
+	 *
+	 * If this method is overridden, `getPreferencesWidget()` will NOT be called.
+	 *
+	 * @param {Adw.PreferencesWindow} window - the preferences window
+	 */
+	fillPreferencesWindow(window) {
+		window.search_enabled = true;
+		window.set_default_size(500, 630);
+
+	  let preferencesPage = new Preferences({ settings: this.getSettings(`${this.metadata['settings-schema']}.time`) })
+	  window.add(preferencesPage);
+	}
 }
